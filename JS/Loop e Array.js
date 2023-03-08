db49ec8a65b3a5f9691a92a8e2ccbc9347ad806a
kaiqueAlbano <kaiquealbano@gmail.com>
2023-03-08 14:06:04 -0300

    JS

--- JS/Loop e Array.js
+++ JS/Loop e Array.js
@@ -1,4 +1,4 @@
-
+//USANDO O FOR NO JS
 var videoGames = ['Switch', 'PS4', 'XBOX', '3DS'];
 for(var item = 0; item < videoGames.length; item++) {
   console.log(videoGames[item]);
@@ -7,6 +7,7 @@ for(var item = 0; item < videoGames.length; item++) {
   }
 
 
+//USANDO O FOREACH NO JS
 var frutas = ['Banana', 'Pera', 'Maçã', 'Abacaxi', 'Uva'];
 frutas.forEach(function(fruta, index, array) {
   frutas.pop(); /*Remover um item*/
